28f3c43988995344bf9b64719cd3e9b3cf65c8a5
Yooinhak <syu3236@gmail.com>
2022-06-29 16:55:03 +0900

    Addbuild folder

--- src/containers/ChangeJsonContainer.js
+++ src/containers/ChangeJsonContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { change_lang } from '../modules/json';
 import Selector from '../components/LanguageChoicePopIp/Selector';
 
