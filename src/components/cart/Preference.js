5221e9d9cd4e0ea27bffd6926e6af0b8641239b3
Yooinhak <syu3236@gmail.com>
2022-07-07 12:04:08 +0900

    Done Preference translation function
    카트 추가 요청사항 번역기능 완료

--- src/components/cart/Preference.js
+++ src/components/cart/Preference.js
@@ -2,20 +2,9 @@ import React, { useEffect, useState } from 'react';
 import '../../scss/cart.scss';
 import axios from 'axios';
 
-const PrefContainer = ({ food }) => {
+const PrefItemContainer = ({ food }) => {
   return (
-    // <div className='foodPrefBox'>
-    //   {food.name}
-    //   {food.pref.map((foodPref) => {
-    //     return (
-    //       <div key={foodPref.id} className='prefItem'>
-    //         {foodPref.text ? `-> ${foodPref.text}` : null}
-    //       </div>
-    //     );
-    //   })}
-    // </div>
     <div className='foodPrefBox'>
-      {console.log(food)}
       {food ? food.split(':')[0] : null}
       {food
         ? food
@@ -24,7 +13,7 @@ const PrefContainer = ({ food }) => {
             .map((value, index2) => {
               return (
                 <div key={index2} className='prefItem'>
-                  {value ? `-> ${value}` : null}
+                  {value.trim() ? `-> ${value}` : null}
                 </div>
               );
             })
@@ -34,38 +23,14 @@ const PrefContainer = ({ food }) => {
 };
 
 const Preference = ({ currentFoods, currentLang }) => {
-  const [allFoodPref, setAllFoodPref] = useState('');
-  const [allFoodPrefEng, setAllFoodPrefEng] = useState('');
-  const [Lang, setLang] = useState(true);
+  const [mkSentences, setMkSentences] = useState('');
+  const [anotherLangSentences, setanotherLangSentences] = useState('');
+  const [reverseLang, setreverseLang] = useState(false);
 
+  // 한 번에 번역할 수 있게 문장화 시키기.
+  // => 카트에 담겨온 state가 변할때마다 바꿔주기.
   useEffect(() => {
-    allFoodPref
-      ? axios
-          .post(
-            '/api/v1/papago/n2mt',
-            {
-              source: 'ko',
-              target: 'en',
-              text: allFoodPref,
-            },
-            {
-              headers: {
-                'X-Naver-Client-Id': 'Yvknb_Q5yHFA8mxRbwUQ',
-                'X-Naver-Client-Secret': 'kNfQu56UpW',
-              },
-            },
-          )
-          .then((response) => {
-            setAllFoodPrefEng(response.data.message.result.translatedText);
-          })
-          .catch((e) => {
-            console.log(e);
-          })
-      : setAllFoodPrefEng('');
-  }, [allFoodPref]);
-
-  useEffect(() => {
-    setAllFoodPref(
+    setMkSentences(
       currentFoods
         .map((food) => {
           return [
@@ -79,26 +44,54 @@ const Preference = ({ currentFoods, currentLang }) => {
               .join('+'),
           ].join(':');
         })
-        .join(','),
+        .join('='),
     );
   }, [currentFoods]);
 
+  // 문장화 시킨 문장을 papago API로 번역해서
+  // anotherLangSentences에 넣어주기.
+  // 계속 번역을 돌리는것 보다는 state에 넣어 두고 사용하는게 좋을것 같다는 판단.
+  useEffect(() => {
+    mkSentences
+      ? axios
+          .post(
+            '/api/v1/papago/n2mt', //proxy target: 'https://openapi.naver.com'
+            {
+              source: currentLang === 'kr' ? 'ko' : 'en',
+              target: currentLang === 'kr' ? 'en' : 'ko',
+              text: mkSentences,
+            },
+            {
+              headers: {
+                'X-Naver-Client-Id': 'Yvknb_Q5yHFA8mxRbwUQ',
+                'X-Naver-Client-Secret': 'kNfQu56UpW',
+              },
+            },
+          )
+          .then((response) => {
+            setanotherLangSentences(
+              response.data.message.result.translatedText,
+            );
+          })
+          .catch((e) => {
+            console.log(e);
+          })
+      : setanotherLangSentences('');
+  }, [mkSentences, currentLang]);
+
   return (
-    // <div className='prefContainer'>
-    //   <button onClick={() => console.log(allFoodPref)}>ggg</button>
-    //   {currentLang === 'kr' ? '추가 요청사항' : 'Preference and Instructions'}
-    //   <div className='prefInfo'>
-    //     {currentFoods.map((food) => {
-    //       return <PrefContainer key={food.id} food={food} />;
-    //     })}
-    //   </div>
-    // </div>
     <div className='prefContainer'>
-      <button onClick={() => setLang(!Lang)}>번역해서 보여드리기!!</button>
+      <button onClick={() => setreverseLang(!reverseLang)}>
+        번역해서 보여드리기!!
+      </button>
       <div className='prefInfo'>
-        {(Lang ? allFoodPref : allFoodPrefEng).split(',').map((food, index) => {
-          return <PrefContainer key={index} food={food} />;
-        })}
+        {console.log(mkSentences)}
+        {console.log(anotherLangSentences)}
+        {(reverseLang ? anotherLangSentences : mkSentences)
+          .split('=')
+          .map((food, index) => {
+            return <PrefItemContainer key={index} food={food} />;
+          })}
       </div>
     </div>
   );
